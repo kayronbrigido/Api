7978e37a0f04234295a6743add7674b0058fdbfa
Kayron Wesley <setokayron@hotmail.com>
2021-06-21 23:53:55 -0300

    Removed ./ in imagem

--- api.js
+++ api.js
@@ -2,60 +2,60 @@
     {"id": 1,
     "nome": "Yu-Gi-Oh!",
     "sinopse": "Tudo muda para o jovem Yugi Muto e seus amigos Joey, Tristan e Téa quando a vida deles passa a girar em torno de um jogo de cartas fantásticas.",
-    "imagem": "./img/yugioh.jpg"
+    "imagem": "img/yugioh.jpg"
     },
 
     {"id": 2,
     "nome": "Bleach",
     "sinopse": "Após o jovem Ichigo Kurosaki adquirir superpoderes da ceifeira de almas Rukia Kuchiki, os dois unem forças para ajudar almas perdidas",
-    "imagem": "./img/bleach.jpg"
+    "imagem": "img/bleach.jpg"
     },
 
     {"id": 3,
     "nome": "One-Punch Man",
     "sinopse": "O super-herói mais forte do mundo pode matar qualquer um com um só golpe. Mas, com uma vida sem desafios, ele sofre com o tédio e a depressão.",
-    "imagem": "./img/onepunch.jpg"
+    "imagem": "img/onepunch.jpg"
     },
 
     {"id": 4,
     "nome": "DEATH NOTE",
     "sinopse": "Um estudante japonês encontra um caderno misterioso e descobre ter poderes para matar todas as pessoas cujos nomes forem escritos no caderno.",
-    "imagem": "./img/deathnote.jpg"
+    "imagem": "img/deathnote.jpg"
     },
 
     {"id": 5,
     "nome": "Demon Slayer: Kimetsu no Yaiba",
     "sinopse": "Demônios massacraram sua família e amaldiçoaram sua irmã. Agora Tanjiro inicia sua jornada para encontrar a cura e se vingar.",
-    "imagem": "./img/demonslayer.jpg"
+    "imagem": "img/demonslayer.jpg"
     },
 
     {"id": 6,
     "nome": "Fullmetal Alchemist: Brotherhood",
     "sinopse": "Com seus corpos em péssimo estado, os irmãos Edward e Alphonse enfrentam forças sinistras para recuperá-los.",
-    "imagem": "./img/fma.jpg"
+    "imagem": "img/fma.jpg"
     },
 
     {"id": 7,
     "nome": "Shokugeki no Soma",
     "sinopse": "Novato em uma prestigiosa academia culinária, o jovem chef Soma precisa vencer batalhas acirradas na cozinha para evitar a expulsão.",
-    "imagem": "./img/shokugeki.jpg"
+    "imagem": "img/shokugeki.jpg"
     },
 
     {"id": 8,
     "nome": "Tenku Shinpan - Sem Saída",
     "sinopse": "A estudante Yuri se vê no alto de um arranha-céu em um mundo onde precisa enfrentar assassinos mascarados dispostos a tudo para matar suas vítimas.",
-    "imagem": "./img/tenkushinpan.jpg"
+    "imagem": "img/tenkushinpan.jpg"
     },
 
     {"id": 9,
     "nome": "GANTZ:O",
     "sinopse": "Recém-mortos são ressuscitados e munidos de armas altamente tecnológicas. Juntos, eles devem derrotar um exército de monstros que ameaçam Tóquio e Osaka.",
-    "imagem": "./img/gantz0.jpg"
+    "imagem": "img/gantz0.jpg"
     },
 
     {"id": 10,
     "nome": "Blue Exorcist",
     "sinopse": "Determinado a se livrar da maldição de ser filho ilegítimo de Satã, Rin segue os passos de seu mentor e se alista na Academia Vera Cruz para se tornar exorcista.",
-    "imagem": "./img/blueexorcist.jpg"
+    "imagem": "img/blueexorcist.jpg"
     }
 ]
